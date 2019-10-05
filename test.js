8fe6af4f0a3a642818c14848e1765e3991047730
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-05 23:26:02 +0200

    Initial commit - added index.html and test.js checkin what age you are

---
+++ test.js
@@ -0,0 +1,12 @@
+var myAge = 39;
+
+if (myAge > 30) {
+    document.write("you are over 30yo"); 
+} else if (myAge > 20){
+    document.write("you are over 20");
+} else if (myAge > 10){
+    document.write("you are over 10");
+}
+else {
+    document.write("you are not over 10 yo");
+}
